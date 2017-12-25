aec096843246b6d1690c8f2072bb995fecc821d3
YunSeobLeeCSE219 <yunseob.lee@stonybrook.edu>
2017-12-24 21:00:44 -0500

    Initial Files

---
+++ DjangoTest/blog/static/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $('button').click(function() {
+    var message = $("#inputMessage").val();
+    console.log(message);
+    $('.Chat').append("<div class='userMessage'>" + message + "</div>");
+    $('#inputMessage').val("");
+    $.ajax({
+        type: "POST",
+        url: "post/",  // or just url: "/my-url/path/"
+        data: {
+            userMessage: message
+        },
+        success: function(data) {
+            $('.Chat').append("<div class='botMessage'>fuckyou</div>");
+        },
+        error: function(xhr, textStatus, errorThrown) {
+            alert("Please report this error: "+errorThrown+xhr.status+xhr.responseText);
+        }
+    });
+  });
+});
