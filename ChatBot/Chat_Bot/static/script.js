7c48e36718dbe97afdc9966159ead07a00698bda
YunSeobLeeCSE219 <yunseob.lee@stonybrook.edu>
2017-12-28 17:13:00 -0500

    Build UI

--- ChatBot/Chat_Bot/static/script.js
+++ ChatBot/Chat_Bot/static/script.js
@@ -1,22 +1,46 @@
 $(document).ready(function() {
-  $('button').click(function() {
-    var message = $("#inputMessage").val();
-    console.log(message);
-    $('.Chat').append("<div class='userMessage'>" + message + "</div>");
-    $('#inputMessage').val("");
-    $.ajax({
-        type: "POST",
-        url: "post/",  // or just url: "/my-url/path/"
-        data: {
-            userMessage: message,
-            'csrfmiddlewaretoken': '{{ csrf_token }}',
-        },
-        success: function(data) {
-            $('.Chat').append("<div class='botMessage'>fuckyou</div>");
-        },
-        error: function(xhr, textStatus, errorThrown) {
-            alert("Please report this error: "+errorThrown+xhr.status+xhr.responseText);
-        }
+    var selection;
+    $(".Input").hide();
+
+    writeMessage("Hello, I'm fucking chatbot! Please select the type of information you want!",0);
+
+    $(".selectBox").click(function () {
+       $('.Input').show();
+       $('.selectBox').hide();
+        console.log($(this).text());
+
+        selection = $(this).text();
+        writeMessage("You choose " + selection + ". Ask a question!",0);
     });
-  });
+
+    $('#sendButton').click(function() {
+        var message = $("#inputMessage").val();
+        console.log(message);
+        writeMessage(message, 1);
+
+        $('#inputMessage').val("");
+        $.ajax({
+            type: "GET",
+            url: "post/",  // or just url: "/my-url/path/"
+            data: {
+                userMessage: message,
+                'csrfmiddlewaretoken': '{{ csrf_token }}',
+                userSelect: selection
+            },
+            success: function(data) {
+                writeMessage("fuck you", 0)
+            },
+            error: function(xhr, textStatus, errorThrown) {
+                alert("Please report this error: "+errorThrown+xhr.status+xhr.responseText);
+            }
+        });
+    });
+    
+    function writeMessage(message, target) {
+        if (target == 0) {
+            $('.Chat').append("<div class='botMessage'>" + message + "</div>");
+        } else {
+            $('.Chat').append("<div class='userMessage'>" + message + "</div>");
+        }
+    }
 });
--- ChatBot/Chat_Bot/views.py
+++ ChatBot/Chat_Bot/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from textblob import TextBlob
 
 # Create your views here.
 def post_list(request):
@@ -7,6 +8,10 @@ def post_list(request):
 
 def processRequest(request):
     if (request.is_ajax()):
+        message = request.GET.get("userMessage")
+        selection = request.GET.get("userSelect")
+        textBlobSentence = TextBlob(message)
+        print(message)
         return HttpResponse("hello!")
     else:
         raise Http404
