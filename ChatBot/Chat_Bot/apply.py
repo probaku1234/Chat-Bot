5c2968f6a931b221552b633cecf2d787e99a6dd1
Kai Ching <kaiching.suen@stonybrook.edu>
2018-01-16 23:10:55 -0500

    UPDATE CLASSES - all application classes show method currently return a list of string - apply class will call the the relative student show methods

--- ChatBot/Chat_Bot/apply.py
+++ ChatBot/Chat_Bot/apply.py
@@ -1,24 +1,40 @@
 from .application import international_application, transfer_application, freshman_application
 from .application import application_Controller
+from .application import words_list
 
 class apply():
     message = "What is your type of student? (transfer, international, freshman)"
-    category = ""
     controller = None
+    student = None
 
     def createStudent(self, studentType):
         if (studentType == "transfer"):
             self.student = transfer_application.transfer()
         elif (studentType == "international"):
             self.student = international_application.international()
-        elif (studentType == "freshman"):
+        else:
             self.student = freshman_application.freshman()
         self.controller = application_Controller.application_Controller(self.student)
-        self.category = studentType
 
     # we need a method to recognize from user input -> our expected input
     #def executeCommand(self, command):
     #    return self.student.show(command)
 
     def executeCommand(self, command):
-        self.controller.search(command)
+        temp = None
+        if(command == words_list.words_list.process):
+            temp = self.student.showApplicationProcess()
+        elif(command == words_list.words_list.requiredDocs):
+            temp = self.student.showRequiredDocuments()
+        elif(command == words_list.words_list.contact):
+            temp = self.student.showContact()
+        elif(command == words_list.words_list.criteria):
+            temp = self.student.showAdmissionCriteria()
+        elif(command == words_list.words_list.deadlines):
+            temp = self.student.showDeadlines()
+        if(temp != None):
+            keywords = temp
+        else:
+            keywords = command
+        for keyword in keywords:
+            self.controller.search(keyword)
